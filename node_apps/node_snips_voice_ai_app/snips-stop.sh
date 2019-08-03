bd52d1bdadb83bf5666070784a17bbb962d8b517
Kaleem <kamleap2019@gmail.com>
2019-08-02 17:11:27 -0700

    Snip Voice Engine based application

---
+++ node_apps/node_snips_voice_ai_app/snips-stop.sh
@@ -0,0 +1,7 @@
+brew services stop snips-dialogue
+brew services stop snips-asr
+brew services stop snips-nlu
+brew services stop snips-tts
+brew services stop snips-hotword
+brew services stop snips-audio-server
+brew services stop mosquitto
